db358cd68a94a00da61ada313b38ea5300cadfb4
valentinesamuel <valentinesamuel2580@gmail.com>
2022-10-19 19:20:07 +0100

    FEAT: Added linted files outside husky filder

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,12 @@
-import express, { Application, NextFunction, Request, Response } from 'express'
+import express, {Application, NextFunction, Request, Response} from 'express';
 
-const app: Application = express()
-const PORT: Number = 3000
+const app: Application = express();
+const PORT: Number = 3000;
 
 app.use('/', (_req: Request, res: Response, _next: NextFunction) => {
-  res.status(200).send({ data: 'Hello from my own personal server' })
-})
+	res.status(200).send({data: 'Hello from my own personal server'});
+});
 
 app.listen(PORT, () => {
-  console.log(`Server is Listening on port ${PORT}...`)
-})
+	console.log(`Server is Listening on port ${PORT}...`);
+});
